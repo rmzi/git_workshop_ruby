1bccc5850b06803dcc6883e6a2d06ef3a4f38baf
rmzi <ramzi.abdoch@gmail.com>
2017-01-17 23:58:13 -0500

    added tests for addition, subtraction, multiplcation, and division

---
+++ spec/math_tests.rb
@@ -0,0 +1,41 @@
+import "math.rb"
+
+describe MathTest do
+
+  # Test Addition Function
+  describe "Addition" do
+    context "given x = 4 and y = 2" do
+      it "returns five" do
+        expect(Math.+(4,2)).to eql(6)
+      end
+    end
+  end
+
+  # Test Subtraction Function
+  describe "Subtraction" do
+    context "given x = 4 and y = 2" do
+      it "returns five" do
+        expect(Math.-(4,2)).to eql(2)
+      end
+    end
+  end
+
+  # Test Multiplication Function
+  describe "Multiplication" do
+    context "given x = 4 and y = 2" do
+      it "returns five" do
+        expect(Math.*(4,2)).to eql(8)
+      end
+    end
+  end
+
+  # Test Division Function
+  describe "Division" do
+    context "given x = 4 and y = 2" do
+      it "returns five" do
+        expect(Math.-(4,2)).to eql(2)
+      end
+    end
+  end
+
+end
